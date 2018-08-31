6b3972504b3935daa9ffa87918858138cb2b842a
Jhon Nova <jhonfredynova@Jhons-MacBook-Pro.local>
2018-08-31 11:10:29 -0500

    add sidebar to home to place the posts from reddit

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react'
+import Sidebar from "react-sidebar"
 import { connect } from 'react-redux'
+import classnames from 'classnames'
 import PropTypes from 'prop-types'
+import { set } from 'lodash'
 import { hideLoading, showLoading, setMessage } from 'actions/appActions'
 import NavigationBar from 'components/navigationBar'
 import Seo from 'components/seo'
 import './home.css'
+const sidebarMql = window.matchMedia(`(min-width: 768px)`)
 
 class Home extends Component {
 
   constructor(props){
     super(props)
+    this.handleMediaQuery = this.handleMediaQuery.bind(this)
+    this.handlePressKey = this.handlePressKey.bind(this)
     this.state = {
+      sidebarDocked: sidebarMql.matches,
+      sidebarOpen: false,
       posts: this.props.post.posts.records
     }
   }
@@ -24,7 +32,43 @@ class Home extends Component {
   async componentWillMount(){
     try{
       this.props.dispatch(showLoading())
+      sidebarMql.addListener(this.handleMediaQuery)
+      document.addEventListener('keydown', this.handlePressKey, false)
       this.props.dispatch(hideLoading())
+    }catch(e){
+      this.props.dispatch(setMessage({ type: 'error', message: e.message }))
+      this.props.dispatch(hideLoading())
+    }
+  }
+
+  componentWillUnmount() {
+    try{
+      sidebarMql.removeListener(this.handleMediaQuery)
+      document.removeEventListener('keydown', this.handlePressKey, false)
+    }catch(e){
+      this.props.dispatch(setMessage({ type: 'error', message: e.message }))
+      this.props.dispatch(hideLoading())
+    }
+  }
+
+  async handleChangeState(path, value){
+    this.setState(set(this.state, path, value))
+  }
+
+  async handleMediaQuery(){
+    try{
+      this.setState({ sidebarDocked: sidebarMql.matches, sidebarOpen: false });
+    }catch(e){
+      this.props.dispatch(setMessage({ type: 'error', message: e.message }))
+      this.props.dispatch(hideLoading())
+    }
+  }
+
+  async handlePressKey(e){
+    try{
+      if(e.keyCode===27) {
+        this.setState({ sidebarOpen: false })
+      }
     }catch(e){
       this.props.dispatch(setMessage({ type: 'error', message: e.message }))
       this.props.dispatch(hideLoading())
@@ -34,8 +78,10 @@ class Home extends Component {
   render() {
     return (
       <div id="home">
-        <Seo data={{ title: 'Home', description: 'Welcome', keyword: ['jhonfredynova'], siteName: this.context.t('siteName') }} />
-        <NavigationBar data={{ title: <h1>Home</h1>, subTitle: <h2>Welcome</h2> }} />
+        <Sidebar rootClassName='sidebarRoot' sidebarClassName='sidebar' contentClassName='sidebarContent' overlayClassName='sidebarOverlay' docked={this.state.sidebarDocked} sidebar={<p>Hola</p>} open={this.state.sidebarOpen} onSetOpen={() => this.handleChangeState('sidebarOpen',false)}>
+          <Seo data={{ title: 'Home', description: 'Welcome', keyword: ['jhonfredynova'], siteName: 'React' }} />
+          <NavigationBar data={{ title: <h1>Home</h1>, subTitle: <h2>Welcome</h2>, btnLeft: <button className={classnames({"btn btn-success": true, "hide": this.state.sidebarDocked})} onClick={() => this.handleChangeState('sidebarOpen', !this.state.sidebarOpen)}><i className="glyphicon glyphicon-chevron-right" /></button> }} />
+        </Sidebar>
       </div>
     )
   }
